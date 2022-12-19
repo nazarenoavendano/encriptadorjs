a36b061e706a12c716f7a005b2e2fe83c99a0835
Nazareno <nazareno.jobs@gmail.com>
2022-12-19 18:32:48 -0300

    cambiofuncion

--- javascript/cifrado.js
+++ javascript/cifrado.js
@@ -1,4 +1,4 @@
-function validarTexto(texto) {
+function validaciondelTexto(texto) {
   let mayusculas = /[A-Z]/g;
   let sintexto = "";
   let caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
@@ -9,16 +9,27 @@ function validarTexto(texto) {
     setTimeout(function () {
       inputTextArea.value = "";
     }, 3000); //RESETEAMOS LUEGO DE 3 SEGUNDOS DE MOSTRAR EL MENSAJE
-    return true;
+    return "No pasa la validación.";
   } else if (texto == sintexto) {
     let inputTextArea = document.getElementById("input-texto");
     inputTextArea.value = "El campo está vacio, favor de ingresar un texto.";
     setTimeout(function () {
       inputTextArea.value = "";
     }, 3000); // SI EL TEXTO ESTA VACIO, LE MOSTRAMOS MSJ Y RESET
-    return true;
+    return "No pasa la validación.";
   } else {
-    return false; //SI ESTA TODO OK MANDA UN FALSE
+    return "Pasó la validación."; //SI ESTA TODO OK MANDA UN FALSE
   }
 }
 
+// // AGREGO FUNCION AL CLIC DEL BOTON ENCRIPTAR
+
+// let encriptador = document.getElementById("btn-encriptar")
+
+// encriptador.addEventListener("click", function () {
+//   let textoIngresado = document.getElementById("input-texto");
+//   let texto = textoIngresado.value;
+
+//   if (validarTexto)
+
+// } )

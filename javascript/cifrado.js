d7abbd5cf7b3d268cffb7ab08e9217e8701605d4
Nazareno <nazareno.jobs@gmail.com>
2022-12-20 10:43:18 -0300

    primera función para encriptar el código

--- javascript/cifrado.js
+++ javascript/cifrado.js
@@ -1,3 +1,21 @@
+// USAMOS KEY OBJ VALUES PARA SEPARAR CLAVE VALOR DE UN ARRAY CON LAS REGLAS
 
+const CODIGOARRAY = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
 
+function encriptar(txt) {
+  let msjEncriptado = "";
+  let vocales = Object.values(CODIGOARRAY);
+  let palabras = Object.keys(CODIGOARRAY);
 
+  for (let i = 0; i < vocales.length; i++) {
+    msjEncriptado = txt.replaceAll(palabras[i], vocales[i]);
+    txt = msjEncriptado;
+  }
+  return msjEncriptado;
+}
